86d70f6f3a3664f587e2d280b0afb00c6c0ec4ca
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-03-19 10:02:00 +0100

    add/improve global live-search preview

--- src/components/Toolbar/Toolbar.vue.js
+++ src/components/Toolbar/Toolbar.vue.js
@@ -1,16 +1,15 @@
-import {paths as artistPaths} from './../../ecosystems/vue-router/routes/ArtistsRoutes';
-import {paths as recordPaths} from './../../ecosystems/vue-router/routes/RecordsRoutes';
-import {paths as releasePaths} from './../../ecosystems/vue-router/routes/ReleasesRoutes';
+import Router, {paths} from './../../ecosystems/vue-router/Router';
 import {_} from 'underscore';
-import Router from './../../ecosystems/vue-router/Router';
+import DataLoader from './../../scripts/DataLoader';
+import {onAfterMap} from './../../scripts/DataLoaderUtils';
 
-const relevantPaths = [
-  artistPaths.root,
-  recordPaths.root,
-  releasePaths.root,
-  artistPaths.search,
-  recordPaths.search,
-  releasePaths.search];
+const specificRoutes = [
+  paths.private.artists.root,
+  paths.private.records.root,
+  paths.private.releases.root,
+  paths.private.artists.search,
+  paths.private.records.search,
+  paths.private.releases.search];
 
 export default {
   name: 'Toolbar',
@@ -19,16 +18,34 @@ export default {
     Router,
   },
   
-  data: () => {
-    return {
-      query: '',
-    };
+  mounted: function() {
+    this.loaderArtists.onAfter = onAfterMap(i => Object.assign({
+      icon: 'people',
+      label: i.Name,
+      value: paths.private.artists.lookup.replace(':name',
+        encodeURIComponent(i.Name)),
+    }));
+    this.loaderReleases.onAfter = onAfterMap(i => Object.assign({
+      icon: 'view_agenda',
+      label: i.Title,
+      value: paths.private.releases.lookup.replace(':name',
+        encodeURIComponent(i.Title)),
+    }));
+    this.loaderRecords.onAfter = onAfterMap(i => Object.assign({
+      icon: 'view_list',
+      label: i.Title,
+      value: paths.private.records.lookup.replace(':name',
+        encodeURIComponent(i.Title)),
+    }));
   },
   
-  mounted: function() {
-    if (this.currentRoute && this.$route.params.name) {
-      this.query = this.$route.params.name;
-    }
+  data: () => {
+    return {
+      loaderArtists: new DataLoader('artists/likeName', this),
+      loaderReleases: new DataLoader('releases/likeName', this),
+      loaderRecords: new DataLoader('records/likeName', this),
+      query: '',
+    };
   },
   
   computed: {
@@ -36,14 +53,14 @@ export default {
       let name = encodeURIComponent(this.$route.params.name);
       let fullPath = this.$route.fullPath;
       let match;
-      for (let r in relevantPaths) {
-        let raw = relevantPaths[r];
+      for (let r in specificRoutes) {
+        let raw = specificRoutes[r];
         let custom = raw.replace(':name', name);
         if (custom === fullPath) {
           if (raw.indexOf(':name') < 0) {
             // root, find matching search sub route
-            for (let k in relevantPaths) {
-              let entry = relevantPaths[k];
+            for (let k in specificRoutes) {
+              let entry = specificRoutes[k];
               if (entry.startsWith(raw) && raw !== entry) {
                 match = entry;
                 break;
@@ -57,29 +74,37 @@ export default {
       }
       return match;
     },
-  },
   
-  methods: {
-    updateValue: function(val) {
-      // update route
-      let match = this.currentRoute;
-      if (!match) {
-        return;
-      }
-      match = match.replace(':name', encodeURIComponent(val));
-      Router.push(match);
+    displayResults: function() {
+      return !this.currentRoute && this.query.trim();
     },
   },
   
   watch: {
-    query: _.debounce(function(v) {
-      this.updateValue(v);
-    }, 750),
-  
-    '$route': function() {
-      if (this.query && !this.currentRoute) {
-        this.query = '';
+    query: _.debounce(function(searchTerm) {
+      searchTerm = searchTerm.trim();
+      if (this.currentRoute) {
+        // context search
+        let match = this.currentRoute.replace(':name',
+          encodeURIComponent(searchTerm));
+        Router.push(match);
+        return;
       }
-    },
+      
+      if (!searchTerm) {
+        return;
+      }
+      
+      // search globally
+      this.loaderArtists.reset();
+      this.loaderReleases.reset();
+      this.loaderRecords.reset();
+      
+      let payload = {name: searchTerm, pageSize: 5};
+      let logError = (r) => { console.error(r); };
+      this.loaderArtists.load(payload).catch(logError);
+      this.loaderReleases.load(payload).catch(logError);
+      this.loaderRecords.load(payload).catch(logError);
+    }, 750),
   },
 };
