4e8f61d4de95f3d6936b180e90fc6ee5127e5967
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-08-29 23:30:40 +0200

    added list selection as input type possibility

--- src/components/InputScreen/InputScreen.vue.js
+++ src/components/InputScreen/InputScreen.vue.js
@@ -1,13 +1,30 @@
+import List from './../List/List';
+
+let i = 0;
+export const inputTypes = {
+  buttonChoice: 1 << i++, list: 1 << i++,
+};
+
 export default {
   name: 'InputScreen',
+  
+  components: {
+    List,
+  },
   
   props: {
     data: {},
   },
   
+  data: () => {
+    return {
+      inputTypes,
+    };
+  },
+  
   methods: {
     getStyle: function(btn) {
-      return `md-raised md-${btn.type}`;
+      return btn.type ? `md-raised md-${btn.type}` : 'md-raised';
     },
   },
 };
